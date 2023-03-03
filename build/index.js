5def5dcc9fe3fe9e6a431f53d3ad7a44ff77c1fb
Capdoo <mazher_2014@hotmail.com>
2023-03-03 09:46:16 -0500

    first commit

---
+++ build/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _express = _interopRequireDefault(require("express"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+console.log('Hello world');
+var app = (0, _express["default"])();
+app.listen();
+console.log('Server listen on port', 3000);
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import express from 'express'
+
+console.log('Hello world');
+
+const app = express();
+app.listen();
+console.log('Server listen on port', 3000);
